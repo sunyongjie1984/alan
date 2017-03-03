7433f064f1761e3df9a7f47389790786720b7a1f
SUNYongjie <sunyongjie1984@gmail.com>
2017-03-03 10:51:48 +0800

    first commit by sunyj

---
+++ SalMoreThanAvg.sql
@@ -0,0 +1,20 @@
+-- select deptno, avg(sal) from emp group by deptno;
+
+-- select empno, ename,sal, deptno
+-- from emp e1
+-- where sal >
+-- (
+--     select avg(sal) from emp e2 where e1.deptno = e2.deptno
+-- )
+-- order by deptno;
+
+-- select e.empno, e.ename, e.deptno from emp e, dept d where e.deptno = d.deptno;
+
+select t1.empno, t1.ename, t1.deptno, t1.sal, t2.AvgSal
+from emp t1,
+(select deptno, avg(sal) AvgSal from emp group by deptno) t2
+where
+t1.deptno = t2.deptno
+and
+T1.sal > t2.AvgSal
+order by t1.deptno;
